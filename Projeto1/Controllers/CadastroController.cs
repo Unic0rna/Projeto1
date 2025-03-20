110c632e1d5f62a7df7297107489f589e71ce797
Debora <ds336756@gmail.com>
2025-03-20 15:01:10 -0300

    Criando a controller do cadastro

---
+++ Projeto1/Controllers/CadastroController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Projeto1.Models;
+
+namespace Projeto1.Controllers
+{
+    public class CadastroController : Controller
+    {
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastro(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                _usuarioRepositorio.AdicionarUsuario(usuario);
+                return RedirectToAction("Login");
+            }
+
+            return View(usuario);
+        }
+    }
+}
